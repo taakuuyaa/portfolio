9216447a6fef017bb2e7f53b71756fafe8c08a6a
taakuuyaa <43112014+taakuuyaa@users.noreply.github.com>
2019-08-26 01:34:30 +0900

    mod: index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        portfolio(test build)
+        portfolio(staging)
       </h1>
       <h2 class="subtitle">
         My fabulous Nuxt.js project

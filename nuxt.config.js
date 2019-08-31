7a5790eb441e82ee42f2bc3482ba7370201bf410
taakuuyaa <43112014+taakuuyaa@users.noreply.github.com>
2019-08-31 17:37:36 +0900

    mod: エイリアス解決

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 export default {
   mode: 'universal',
   /*
@@ -11,10 +13,10 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        content: process.env.npm_package_description || '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
    ** Customize the progress-bar color
@@ -33,14 +35,14 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
   ],
   /*
    ** Axios module configuration
@@ -54,6 +56,20 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
+    extend(config, ctx) {},
+  },
+
+  /*
+   * `@` , `~` のエイリアスを解決する
+   * 参考:
+   * https://qiita.com/sutoh/items/54e94df1c610c0f18c85
+   */
+  resolve: {
+    extensions: ['.js', '.json', '.vue', '.ts'],
+    root: path.resolve(__dirname),
+    alias: {
+      '@': path.resolve(__dirname),
+      '~': path.resolve(__dirname),
+    },
+  },
 }

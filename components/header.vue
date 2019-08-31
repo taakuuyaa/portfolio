f9f36a8e76253a82f709be4663a40aecf410bbb1
taakuuyaa <43112014+taakuuyaa@users.noreply.github.com>
2019-08-31 16:20:04 +0900

    add: header&footer

---
+++ components/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <h1>header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+.header {
+  background-color: aquamarine;
+}
+</style>

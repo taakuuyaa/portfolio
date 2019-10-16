7eacdcc14075d581a1ba9295b83f208ad629a2f4
taakuuyaa <43112014+taakuuyaa@users.noreply.github.com>
2019-10-17 01:50:28 +0900

    lintの設定見直し

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,23 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
+    'eslint:recommended',
+    'plugin:vue/recommended',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
+  plugins: ['vue'],
   rules: {
-  }
+    semi: [2, 'never'],
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'prettier/prettier': ['error', { semi: false }],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': 'off',
+  },
 }

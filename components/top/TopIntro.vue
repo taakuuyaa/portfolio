473fdc5fb6d868059dcb00decf6c0d94f1406792
taakuuyaa <43112014+taakuuyaa@users.noreply.github.com>
2019-11-04 16:47:59 +0900

    top 大改修

---
+++ components/top/TopIntro.vue
@@ -0,0 +1,140 @@
+<template>
+  <section class="section center">
+    <div class="container">
+      <div class="center-column">
+        <h3 class="subtitle">HELLO, WORLD.</h3>
+        <h1 class="title">I'm Taakuuyaa.</h1>
+        <p class="occupation"><span>WEB ENGINEER</span></p>
+        <button>
+          LEARN MORE
+          <font-awesome-icon
+            :icon="['fas', 'angle-double-down']"
+            class="fa-1x"
+          />
+        </button>
+      </div>
+    </div>
+    <a href="#"><span></span></a>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TopIntro',
+}
+</script>
+
+<style lang="scss" scoped>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.center-column {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.section {
+  background: #000022 url('~assets/image/circuit.svg');
+  min-height: 100vh;
+  min-width: 100vw;
+  .container {
+    .title,
+    .occupation {
+      color: #fff;
+    }
+
+    .subtitle {
+      color: $focus_color;
+      margin-bottom: 0.3rem;
+      letter-spacing: 0.3rem;
+      font-family: 'Lato', sans-serif;
+      font-size: 1rem;
+    }
+    .title {
+      max-width: 900px;
+      color: #fff;
+      font-size: 2.5rem;
+      font-family: 'Raleway', sans-serif;
+      font-weight: 900;
+      letter-spacing: 0.1rem;
+      margin: 0 auto;
+    }
+    .occupation {
+      letter-spacing: 0.2rem;
+      margin-bottom: 1.2rem;
+      span {
+        font-size: 1.2rem;
+      }
+    }
+    button {
+      color: #fff;
+      border-color: $focus_color;
+      font-size: 1.2rem;
+      line-height: 3.4rem;
+      letter-spacing: 0.3rem;
+      padding: 0 2rem;
+      background: 0 0;
+      transition: all 0.3s ease-in-out;
+      outline: none;
+    }
+    button:hover {
+      background-color: $focus_color;
+      border-color: #fff;
+    }
+  }
+  a {
+    padding-top: 70px;
+    position: absolute;
+    bottom: 10px;
+    z-index: 2;
+    display: block;
+    -webkit-transform: translate(0, -50%);
+    transform: translate(0, -50%);
+    text-decoration: none;
+  }
+  a span {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 40px;
+    height: 40px;
+    margin-left: -20px;
+    border-left: 2px solid $focus_color;
+    border-bottom: 2px solid $focus_color;
+    -webkit-transform: rotateZ(-45deg);
+    transform: rotateZ(-45deg);
+    -webkit-animation: sdb 1.5s infinite;
+    animation: sdb 1.5s infinite;
+    box-sizing: border-box;
+  }
+  @-webkit-keyframes sdb {
+    0% {
+      -webkit-transform: rotateY(0) rotateZ(-45deg) translate(0, 0);
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      -webkit-transform: rotateY(720deg) rotateZ(-45deg) translate(-20px, 20px);
+      opacity: 0;
+    }
+  }
+  @keyframes sdb {
+    0% {
+      transform: rotateY(0) rotateZ(-45deg) translate(0, 0);
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      transform: rotateY(720deg) rotateZ(-45deg) translate(-20px, 20px);
+      opacity: 0;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,150 +1,15 @@
 <template>
-  <div id="wrapper">
-    <div class="profile-wrapper">
-      <div class="profile columns is-mobile">
-        <div class="column">
-          <div class="image is-128x128">
-            <img
-              class="is-rounded"
-              src="~/assets/image/profile_icon.jpeg"
-              alt="icon"
-            />
-          </div>
-        </div>
-
-        <div class="profile-description column">
-          <p>Name: taakuuyaa<br />Age: 24<br />Job: Engineer</p>
-        </div>
-      </div>
-      <hr />
-      <p>
-        コスメ系ECスタートアップでWEBエンジニアをしています。
-        主にフロントエンド(Nuxt.js)・バックエンド(Rails)を担当しています。
-        趣味は酒・映画鑑賞・モータースポーツ・ガジェット集め etc.
-      </p>
-
-      <div class="history">
-        <h1>History</h1>
-        <p class="history-item">
-          2018.07 ~ now
-          <a href="https://noin.tv/" target="_blank" rel="nofollow noopener">
-            NOIN, INC based in Tokyo
-          </a>
-        </p>
-        <p class="history-item">
-          2018.04 ~ 2019.06
-          <a
-            href="https://www.gnavi.co.jp/"
-            target="_blank"
-            rel="nofollow noopener"
-          >
-            Gurunavi, Inc based in Tokyo
-          </a>
-        </p>
-      </div>
-
-      <div class="links">
-        <h1>Accounts</h1>
-        <div class="columns is-mobile">
-          <div class="column">
-            <a
-              href="https://github.com/taakuuyaa"
-              target="_blank"
-              rel="nofollow noopener"
-              aria-label="githubへ"
-            >
-              <font-awesome-icon
-                :icon="['fab', 'github-square']"
-                class="fa-4x"
-              />
-            </a>
-          </div>
-          <div class="column">
-            <a
-              href="https://twitter.com/_taakuuyaa"
-              target="_blank"
-              rel="nofollow noopener"
-              aria-label="twitterへ"
-            >
-              <font-awesome-icon
-                :icon="['fab', 'twitter-square']"
-                class="fa-4x"
-              />
-            </a>
-          </div>
-          <div class="column">
-            <a
-              href="https://www.instagram.com/_taakuuyaa"
-              target="_blank"
-              rel="nofollow noopener"
-              aria-label="instagramへ"
-            >
-              <font-awesome-icon :icon="['fab', 'instagram']" class="fa-4x" />
-            </a>
-          </div>
-          <div class="column">
-            <a
-              href="https://www.facebook.com/profile.php?id=100036937809528"
-              target="_blank"
-              rel="nofollow noopener"
-              aria-label="facebookへ"
-            >
-              <font-awesome-icon
-                :icon="['fab', 'facebook-square']"
-                class="fa-4x"
-              />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <TopIntro />
 </template>
 
 <script>
+import TopIntro from '@/components/top/TopIntro'
+
 export default {
-  layout: 'default',
+  name: 'PageTop',
+  components: { TopIntro },
+  component: { TopIntro },
 }
 </script>
 
-<style lang="scss">
-#wrapper {
-  p {
-    font-size: 1.2rem;
-  }
-  .profile-wrapper {
-    max-width: 800px;
-    font-size: 20px;
-    margin: 0 auto;
-    padding: 24px;
-    .profile {
-      .is-128x128 {
-        margin: auto;
-      }
-      .profile-icon {
-        margin: auto;
-      }
-      .profile-description {
-        display: flex;
-        justify-content: left;
-        align-items: center;
-      }
-    }
-    .history {
-      text-align: center;
-      margin-top: 10px;
-      padding: 0 55px;
-      .history-item {
-        text-align: left;
-      }
-    }
-    .links {
-      text-align: center;
-      margin-top: 10px;
-      a {
-        color: #000000;
-      }
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
